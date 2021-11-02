bab9b5615cad41da9c1eff43516e70da59eee578
margaretmwaura <mwauramargaret1@gmail.com>
2021-11-02 16:08:14 +0300

    setup the vue.config.js to include the resolve-url-loader into webpack

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="home">
-<!--    <img alt="Vue logo" src="../assets/images/logo.png">-->
-
+  <!--    <img alt="Vue logo" src="../assets/images/logo.png">-->
+  <div class="grid-x home">
+    <div class="cell small-12 medium-5 large-5 home_invite">
+         <p>A digital product
+           <br>
+           design agency
+         </p>
+    </div>
+    <div class="cell small-12 medium-7 large-7 home_think">
+      <img src="../assets/images/statics.png"/>
+    </div>
   </div>
 </template>
 
@@ -10,7 +18,6 @@
 
 export default {
   name: 'Home',
-  components: {
-  }
+  components: {}
 }
 </script>
---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  chainWebpack: (config) => {
+    ['vue-modules', 'vue', 'normal-modules', 'normal'].forEach(rule => {
+      config.module.rule('scss')
+        .oneOf(rule)
+        .use('resolve-url-loader')
+        .loader('resolve-url-loader')
+        .before('sass-loader')
+        .end()
+        .use('sass-loader')
+        .loader('sass-loader')
+        .tap(options =>
+          ({...options, sourceMap: true})
+        );
+    });
+  },
+}
