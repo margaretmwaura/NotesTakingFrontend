f1abf996d30607a7fa93f2308f4a5db8c7bec4b0
margaretmwaura <mwauramargaret1@gmail.com>
2021-11-04 20:10:17 +0300

    using of after in css ; font-awesome icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,84 @@
 <template>
   <!--    <img alt="Vue logo" src="../assets/images/logo.png">-->
-  <div class="grid-x home">
-    <div class="cell small-12 medium-5 large-5 home_invite">
-         <p>A digital product
-           <br>
-           design agency
-         </p>
+  <div class="landing">
+    <div class="grid-x landing_home">
+      <div class="cell small-12 medium-5 large-5 landing_home_invite">
+        <p>A digital product
+          <br>
+          design agency
+        </p>
+        <div class="landing_home_invite_links">
+          <button>
+            Our Stores<i class="fas fa-arrow-right"></i>
+          </button>
+          <h6>See the demo </h6>
+        </div>
+      </div>
+      <div class="cell small-12 medium-7 large-7 landing_home_think">
+        <img src="../assets/images/statics.png"/>
+      </div>
     </div>
-    <div class="cell small-12 medium-7 large-7 home_think">
-      <img src="../assets/images/statics.png"/>
+    <div class="grid-x landing_entire">
+      <div class="cell small-12 medium-4 large-4 landing_entire_statistics">
+        <div class="landing_entire_statistics_images">
+          <img src="../assets/images/increase.png">
+        </div>
+        <h5>Business Planning</h5>
+        <p>Our Business plan is a written document
+          <br>
+          describing a company's core business activities
+          <br>
+          objectives and how it plans to achieve
+          <br>
+          its goals
+        </p>
+      </div>
+      <div class="cell small-12 medium-4 large-4 landing_entire_finances">
+        <div class="landing_entire_finances_images">
+          <img src="../assets/images/hand.png">
+        </div>
+        <h5>Financial Planning</h5>
+        <p>Our Business plan is a written document
+          <br>
+          describing a company's core business activities
+          <br>
+          objectives and how it plans to achieve
+          <br>
+          its goals
+        </p>
+      </div>
+      <div class="cell small-12 medium-4 large-4 landing_entire_market">
+        <div class="landing_entire_market_images">
+          <img src="../assets/images/magnifier.png">
+        </div>
+        <h5>Market Analysis</h5>
+        <p>Our Business plan is a written document
+          <br>
+          describing a company's core business activities
+          <br>
+          objectives and how it plans to achieve
+          <br>
+          its goals
+        </p>
+      </div>
+    </div>
+    <div class="grid-x grid-margin-x landing_advertise">
+      <div class="cell small-12 medium-6 large-6 landing_advertise_ad">
+        <h3>Great Digital Agency
+          <br>
+          Since 1980
+        </h3>
+        <h6>Creative and professional digital agency</h6>
+        <p>Our Business plan is a written document describing a company's core business activities
+         0bjectives and how it plans to achieve its goals. Our goal is to provide our client high
+         quality product with modern idea according to their budgets and requirements.
+        </p>
+      </div>
+      <div class="cell small-12 medium-6 large-6 landing_advertise_photos">
+          <div class="landing_advertise_photos_top">
+          </div>
+        <img src="../assets/images/conversation.jpeg">
+      </div>
     </div>
   </div>
 </template>
