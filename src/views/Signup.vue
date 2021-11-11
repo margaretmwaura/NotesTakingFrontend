c8e2e1a2965a48765b071f4d2f6b4f0cc484ad2e
margaretmwaura <mwauramargaret1@gmail.com>
2021-11-11 19:14:38 +0300

    attaching and detaching of classes with vuejs

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -13,6 +13,14 @@
       <div class="cell small-12 medium-6 large-6 ">
         <div class="register_forms">
           <div class="grid-x grid-margin-x grid-margin-y register_forms_signup">
+            <div class="cell small-12 medium-12 large-12 register_forms_signup_switch">
+              <button class="signup" v-on:click="start_signup"
+                      v-bind:class="{ isActive:signup , isNotActive:login }">Signup
+              </button>
+              <button class="login" v-on:click="start_login"
+                      v-bind:class="{ isActive:login , isNotActive:signup }">Login
+              </button>
+            </div>
             <div class="cell small-12 medium-6 large-6">
               <label>Username *</label>
               <input v-model="user.name" placeholder="John">
@@ -25,7 +33,7 @@
               <label>Password *</label>
               <input v-model="user.password" placeholder="Doe">
             </div>
-            <div class="cell small-12 medium-12 large-12">
+            <div class="cell small-12 medium-12 large-12 register_forms_signup_action">
               <button v-on:click="register">
                 Signup
               </button>
@@ -56,40 +64,63 @@ export default {
         email: "",
         password: "",
       },
+      login: false,
+      signup: true
     }
   },
-
   //Note to self mapGetters is not called like a function but like a variable
-  computed:{
+  computed: {
     ...mapGetters(['getToken']),
   },
   methods: {
     async register() {
       await axios.get('/sanctum/csrf-cookie')
-      await axios.post("/register", this.user).then(({data})=>{
+      await axios.post("/register", this.user).then(({data}) => {
         this.$store.dispatch('settoken', data.token)
         this.getCurrentUser(data.token)
-      }).catch(({response:{data}})=>{
+      }).catch(({response: {data}}) => {
         alert(data.message)
-      }).finally(()=>{
+      }).finally(() => {
       })
     },
-    getCurrentUser(token){
+    getCurrentUser(token) {
       // console.log(this.$store.state.token)
       // console.log(this.getToken)
-      axios.get('/api/user',{ headers: {
+      axios.get('/api/user', {
+        headers: {
           'Authorization': `Bearer ${token}`
-        },}).then(({data}) =>{
+        },
+      }).then(({data}) => {
       });
     },
+    start_signup() {
+      this.login = false;
+      this.signup = true;
+    },
+    start_login() {
+      this.login = true;
+      this.signup = false;
+    }
   },
   mounted() {
     console.log("we here")
     console.log(this.$store.state.token)
   },
-  created(){
+  created() {
     console.log("we here")
     console.log(this.$store.state.token)
   }
 }
 </script>
+
+<style>
+.isActive {
+  z-index: 99;
+  opacity: 1;
+}
+
+.isNotActive {
+  z-index: 1;
+  opacity: 0.4;
+}
+</style>
