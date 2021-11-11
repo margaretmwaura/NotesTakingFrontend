6fb42ee7989fc11362cc48094e36d78687d39661
margaretmwaura <mwauramargaret1@gmail.com>
2021-11-11 07:00:02 +0300

    register user

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,42 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+import router from '../router'
 
 export default createStore({
-  state: {
+  state:{
+    authenticated:false,
+    user:{}
   },
-  mutations: {
+  getters:{
+    authenticated(state){
+      return state.authenticated
+    },
+    user(state){
+      return state.user
+    }
   },
-  actions: {
+  mutations:{
+    SET_AUTHENTICATED (state, value) {
+      state.authenticated = value
+    },
+    SET_USER (state, value) {
+      state.user = value
+    }
   },
-  modules: {
+  actions:{
+    login({commit}){
+      return axios.get('/api/user').then(({data})=>{
+        commit('SET_USER',data)
+        commit('SET_AUTHENTICATED',true)
+        router.push({name:'dashboard'})
+      }).catch(({response:{data}})=>{
+        commit('SET_USER',{})
+        commit('SET_AUTHENTICATED',false)
+      })
+    },
+    logout({commit}){
+      commit('SET_USER',{})
+      commit('SET_AUTHENTICATED',false)
+    }
   }
 })
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -14,19 +14,19 @@
         <div class="register_forms">
           <div class="grid-x grid-margin-x grid-margin-y register_forms_signup">
             <div class="cell small-12 medium-6 large-6">
-              <label>First Name *</label>
-              <input v-model="fname" placeholder="John">
+              <label>Username *</label>
+              <input v-model="user.name" placeholder="John">
             </div>
             <div class="cell small-12 medium-6 large-6">
-              <label>Last Name *</label>
-              <input v-model="lname" placeholder="Doe">
+              <label>Email *</label>
+              <input v-model="user.email" placeholder="Doe">
             </div>
             <div class="cell small-12 medium-12 large-12">
               <label>Password *</label>
-              <input v-model="password" placeholder="Doe">
+              <input v-model="user.password" placeholder="Doe">
             </div>
             <div class="cell small-12 medium-12 large-12">
-              <button>
+              <button v-on:click="register">
                 Signup
               </button>
             </div>
@@ -39,12 +39,48 @@
 
 <script>
 
+import {mapActions} from 'vuex'
+import axios from 'axios'
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://localhost:8400"
 export default {
   data() {
     return {
-      fname: "",
-      lname: "",
-      password: ""
+      auth: {
+        email: "",
+        password: ""
+      },
+      user: {
+        name: "",
+        email: "",
+        password: "",
+      },
+    }
+  },
+  methods: {
+    ...mapActions({
+      signIn: 'login'
+    }),
+    // async login(){
+    //   this.processing = true
+    //   await axios.get('/sanctum/csrf-cookie')
+    //   await axios.post('/login',this.auth).then(({data})=>{
+    //     this.signIn()
+    //   }).catch(({response:{data}})=>{
+    //     alert(data.message)
+    //   }).finally(()=>{
+    //
+    //   })
+    // },
+    async register() {
+      await axios.get('/sanctum/csrf-cookie')
+      await axios.post("/register", this.user).then(({data})=>{
+        this.signIn()
+      }).catch(({response:{data}})=>{
+        alert(data.message)
+      }).finally(()=>{
+      })
     }
   }
 }
